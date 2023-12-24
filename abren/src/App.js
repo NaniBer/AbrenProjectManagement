11ba3ed2e75c01d224aff808f575029a1cb04d83
Saron B Yilma <sarinaa.bisrat@gmail.com>
2023-12-24 21:40:57 +0300

    second

--- abren/src/App.js
+++ abren/src/App.js
@@ -1,9 +1,6 @@
 import "./App.css";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import './App.css';
-import React from 'react';
-import { Routes, Route } from "react-router-dom";
 import Form from "./scenes/form/form";
 import Login from "./scenes/form/login";
 // import Dashboard from "./scenes/dashboard";
@@ -12,12 +9,11 @@ import Topbar from "./scenes/global/Topbar";
 // import Sidebar from "./scenes/global/Sidebar";
 import SidebarAdmin from "./scenes/global/SidebarAdmin";
 import Createproject from "./scenes/createproject";
-import Createteamaccount from "./scenes/createteamaccount";
+// import Createteamaccount from "./scenes/createteamaccount";
 import Calendar from "./scenes/calendar";
 import Kanban from "./scenes/kanban";
 import Team from "./scenes/team";
 import Viewuser from "./scenes/viewuser";
-import Updateuser from "./scenes/updateuser";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 
@@ -39,33 +35,12 @@ function App() {
               <Route path="/team" element={<Team />}></Route>
               <Route path="/viewuser" element={<Viewuser />}></Route>
               <Route path="/createproject" element={<Createproject />}></Route>
-              <Route
+              {/* <Route
                 path="/createteamaccount"
                 element={<Createteamaccount />}
-              ></Route>
+              ></Route> */}
               <Route path="/login" element={<Login />} />
             </Routes>
-    <div className="App">
-      <SidebarAdmin />
-      <main className='content'>
-        <Topbar />
-      <Routes>
-      <Route path="/" element={<DashboardAdmin />}></Route>
-      <Route path="/form" element={<Form />}></Route>
-      <Route path="/calendar" element={<Calendar />}></Route>
-      <Route path="/kanban" element={<Kanban />}></Route>
-      <Route path="/team" element={<Team />}></Route>
-      <Route path="/viewuser" element={<Viewuser />}></Route>
-      <Route path="/updateuser/:rowId" element={<Updateuser />}></Route>
-      <Route path="/createproject" element={<Createproject />}></Route>
-      <Route path="/createteamaccount" element={<Createteamaccount/>}></Route>
-
-
-
-
-
-
-      </Routes>
 
             {/* <p>hello</p> */}
           </main>
